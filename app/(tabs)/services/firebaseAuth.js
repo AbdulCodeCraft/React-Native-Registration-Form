a45afe1e6e134bde9af923d2d7e05c97b6d1d68e
AbdulCodeCraft <ar.abdulrahman.a@gmail.com>
2024-08-26 15:28:40 +0530

    firebase authentication done

---
+++ app/(tabs)/services/firebaseAuth.js
@@ -0,0 +1,14 @@
+import { initializeApp } from '@react-native-firebase/app';
+import auth from '@react-native-firebase/auth';
+
+// Your Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAkTR-PSnLQbdwWVEHG-ohZy6pFkQAGby8",
+  authDomain: "resgistration-form-e6233.firebaseapp.com",
+  projectId: "resgistration-form-e6233",
+  storageBucket: "resgistration-form-e6233.appspot.com",
+  messagingSenderId: "847781507735",
+  appId: "1:847781507735:web:f461735dbb2134a1ba2fd2"
+};
+
+initializeApp(firebaseConfig);
